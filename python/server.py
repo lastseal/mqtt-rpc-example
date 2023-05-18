9e15dbbc66e549f40e18073d02bbf32ab5d9b7cd
Rodrigo Arriaza <rarriaza@lastseal.com>
2023-05-18 15:55:38 -0400

    Update server.py

--- python/server.py
+++ python/server.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from micro import mqtt
+
+@mqtt.rpc("status")
+def status(data):
+    return "OK"
+  
+@mqtt.rpc("change")
+def status(data):
+    return "OK"
